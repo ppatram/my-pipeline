4dc4c4092058eeacb35e04531b9a33043c8bafa4
joey <joe@ail.com>
2025-01-30 15:21:00 -0500

    initial

---
+++ bin/pipeline-lambda.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { MyPipelineStack } from '../lib/my-pipeline-stack';
+
+const app = new cdk.App();
+new MyPipelineStack(app, 'MyPipelineStack', {
+  env: {
+    account: '676206914581',
+    region: 'eu-east-1',
+  }
+});
+
+app.synth();
