c17d8d00b3c44e41423ef668791eaea0227180bc
joey <joe@ail.com>
2025-02-06 10:50:58 -0500

    got the github action to complete

--- lib/codepipeline-build-deploy-stack.ts
+++ lib/codepipeline-build-deploy-stack.ts
@@ -1,6 +1,5 @@
 import * as cdk from "aws-cdk-lib";
 import * as codebuild from "aws-cdk-lib/aws-codebuild";
-//import * as codecommit from "aws-cdk-lib/aws-codecommit";
 import * as codedeploy from "aws-cdk-lib/aws-codedeploy";
 import * as pipeline from "aws-cdk-lib/aws-codepipeline";
 import * as pipelineactions from "aws-cdk-lib/aws-codepipeline-actions";
@@ -16,27 +15,17 @@ import * as path from "path";
 import * as fs from 'fs';
 import { Asset } from 'aws-cdk-lib/aws-s3-assets';
 import { IgnoreMode } from 'aws-cdk-lib';
-//import { Code } from 'aws-cdk-lib/aws-codecommit';
-import * as github from 'aws-cdk-lib/aws-codepipeline-actions';
 import * as secrets from 'aws-cdk-lib/aws-secretsmanager';
-import { isString } from "util";
+import * as connection from 'aws-cdk-lib/aws-codestarconnections';
+import * as codestar  from 'aws-cdk-lib/aws-codestar';
 
-export class CodepipelineBuildDeployStack extends cdk.Stack {
+
+export class CodepipelineBuildDeployStackTest extends cdk.Stack {
   
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    
-    const gitHubToken = secrets.Secret.fromSecretAttributes(this, "ImportedSecret", {
-      secretCompleteArn:
-         "arn:aws:secretsmanager:us-east-1:676206914581:secret:github-token-i7U04G"
-
-    });
-
-
-
-
-    // modify gitignore file to remove unneeded files from the github copy    
+    // modify gitignore file to remove unneeded files from the git copy    
     let gitignore = fs.readFileSync('.gitignore').toString().split(/\r?\n/);
     gitignore.push('.git/');
     gitignore = gitignore.filter(g => g != 'node_modules/');
@@ -48,233 +37,42 @@ export class CodepipelineBuildDeployStack extends cdk.Stack {
       exclude: gitignore,
     });
 
-    const codeRepo = new github.GitHubSourceAction({
+    /*
+    const codeStarConnection = new connection.CfnConnection(this, 'codeStarConnection', {
+      connectionName: 'github-connection',      
+    });
+
+    */
+
+    const githubSourceAction= new pipelineactions.CodeStarConnectionsSourceAction({
+      actionName: 'githubSourceAction',
+      connectionArn: 'arn:aws:codeconnections:us-east-1:676206914581:connection/38a41c14-fcf2-44f5-8784-908f4c33cc4d',
       owner: 'ppatram',
       repo: 'my-pipeline',
       branch: 'main',
-      actionName: 'gitHubSource',
-      oauthToken: gitHubToken.secretValueFromJson('token'),
-      output: new pipeline.Artifact(),
-      runOrder: 1,
-      //trigger: github.GitHubTrigger.WEBHOOK
-       });
-
-    
-
-    // Creates an Elastic Container Registry (ECR) image repository
-    const imageRepo = new ecr.Repository(this, "imageRepo");
-
-    // Creates a Task Definition for the ECS Fargate service
-    const fargateTaskDef = new ecs.FargateTaskDefinition(
-      this,
-      "FargateTaskDef"
-    );
-    fargateTaskDef.addContainer("container", {
-      containerName: "web",
-      image: ecs.ContainerImage.fromEcrRepository(imageRepo),
-      portMappings: [{ containerPort: 80 }],
-    });
+      output: new pipeline.Artifact("SourceArtifact"),
+      triggerOnPush: true,
+      });
 
 
-    // CodeBuild project that builds the Docker image
-    const buildImage = new codebuild.Project(this, "BuildImage", {
-      buildSpec: codebuild.BuildSpec.fromSourceFilename("app/buildspec.yaml"),
-      source: codebuild.Source.gitHub({
-        owner: 'ppatram',
-        repo: 'my-pipeline'
-      }),
-      environment: {
-        privileged: true,
-        environmentVariables: {
-          AWS_ACCOUNT_ID: { value: process.env?.CDK_DEFAULT_ACCOUNT || "" },
-          REGION: { value: process.env?.CDK_DEFAULT_REGION || "" },
-          IMAGE_TAG: { value: "latest" },
-          IMAGE_REPO_NAME: { value: imageRepo.repositoryName },
-          REPOSITORY_URI: { value: imageRepo.repositoryUri },
-          TASK_DEFINITION_ARN: { value: fargateTaskDef.taskDefinitionArn },
-          TASK_ROLE_ARN: { value: fargateTaskDef.taskRole.roleArn },
-          EXECUTION_ROLE_ARN: { value: fargateTaskDef.executionRole?.roleArn },
-        },
-      },
-    });
-    
-    // CodeBuild project that builds the Docker image
-    const buildTest = new codebuild.Project(this, "BuildTest", {
-      buildSpec: codebuild.BuildSpec.fromSourceFilename("buildspec.yaml"),
-      source: codebuild.Source.gitHub({
-        owner: 'ppatram',
-        repo: 'my-pipeline'
-      }),
-      environment: {
-        buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_4,  
-      }
-    });
-
-    // Grants CodeBuild project access to pull/push images from/to ECR repo
-    imageRepo.grantPullPush(buildImage);
-
-    // Lambda function that triggers CodeBuild image build project
-    const triggerCodeBuild = new lambda.Function(this, "BuildLambda", {
-      architecture: lambda.Architecture.ARM_64,
-      code: lambda.Code.fromAsset("./lambda"),
-      handler: "trigger-build.handler",
-      runtime: lambda.Runtime.NODEJS_18_X,
-      environment: {
-        REGION: process.env.CDK_DEFAULT_REGION!,
-        CODEBUILD_PROJECT_NAME: buildImage.projectName,
-      },
-      // Allows this Lambda function to trigger the buildImage CodeBuild project
-      initialPolicy: [
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          actions: ["codebuild:StartBuild"],
-          resources: [buildImage.projectArn],
-        }),
-      ],
-    });
-
-    // Triggers a Lambda function using AWS SDK
-    const triggerLambda = new custom.AwsCustomResource(
-      this,
-      "BuildLambdaTrigger",
-      {
-        installLatestAwsSdk: true,
-        policy: custom.AwsCustomResourcePolicy.fromStatements([
-          new iam.PolicyStatement({
-            effect: iam.Effect.ALLOW,
-            actions: ["lambda:InvokeFunction"],
-            resources: [triggerCodeBuild.functionArn],
-          }),
-        ]),
-        onCreate: {
-          service: "Lambda",
-          action: "invoke",
-          physicalResourceId: custom.PhysicalResourceId.of("id"),
-          parameters: {
-            FunctionName: triggerCodeBuild.functionName,
-            InvocationType: "Event",
-          },
-        },
-        onUpdate: {
-          service: "Lambda",
-          action: "invoke",
-          parameters: {
-            FunctionName: triggerCodeBuild.functionName,
-            InvocationType: "Event",
-          },
-        },
-      }
-    );
-
-    // Creates VPC for the ECS Cluster
-    const clusterVpc = new ec2.Vpc(this, "ClusterVpc", {
-      ipAddresses: ec2.IpAddresses.cidr("10.50.0.0/16"),
-    });
-
-    // Deploys the cluster VPC after the initial image build triggers
-    clusterVpc.node.addDependency(triggerLambda);
-
-    // Creates a new blue Target Group that routes traffic from the public Application Load Balancer (ALB) to the
-    // registered targets within the Target Group e.g. (EC2 instances, IP addresses, Lambda functions)
-    // https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-target-groups.html
-    const targetGroupBlue = new elb.ApplicationTargetGroup(
-      this,
-      "BlueTargetGroup",
-      {
-        targetGroupName: "alb-blue-tg",
-        targetType: elb.TargetType.IP,
-        port: 80,
-        vpc: clusterVpc,
-      }
-    );
-
-    // Creates a new green Target Group
-    const targetGroupGreen = new elb.ApplicationTargetGroup(
-      this,
-      "GreenTargetGroup",
-      {
-        targetGroupName: "alb-green-tg",
-        targetType: elb.TargetType.IP,
-        port: 80,
-        vpc: clusterVpc,
-      }
-    );
-
-    // Creates a Security Group for the Application Load Balancer (ALB)
-    const albSg = new ec2.SecurityGroup(this, "SecurityGroup", {
-      vpc: clusterVpc,
-      allowAllOutbound: true,
-    });
-    albSg.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(80),
-      "Allows access on port 80/http",
-      false
-    );
-
-    // Creates a public ALB
-    const publicAlb = new elb.ApplicationLoadBalancer(this, "PublicAlb", {
-      vpc: clusterVpc,
-      internetFacing: true,
-      securityGroup: albSg,
-    });
-
-    // Adds a listener on port 80 to the ALB
-    const albListener = publicAlb.addListener("AlbListener80", {
-      open: false,
-      port: 80,
-      defaultTargetGroups: [targetGroupBlue],
-    });
-
-    // Creates an ECS Fargate service
-    const fargateService = new ecs.FargateService(this, "FargateService", {
-      desiredCount: 1,
-      serviceName: "fargate-frontend-service",
-      taskDefinition: fargateTaskDef,
-      cluster: new ecs.Cluster(this, "EcsCluster", {
-        enableFargateCapacityProviders: true,
-        vpc: clusterVpc,
-      }),
-      // Sets CodeDeploy as the deployment controller
-      deploymentController: {
-        type: ecs.DeploymentControllerType.CODE_DEPLOY,
-      },
-    });
-
-    // Adds the ECS Fargate service to the ALB target group
-    fargateService.attachToApplicationTargetGroup(targetGroupBlue);
-
-    // Creates new pipeline artifacts
-    const sourceArtifact = new pipeline.Artifact("SourceArtifact");
-    const buildArtifact = new pipeline.Artifact("BuildArtifact");
-
+     
     // Creates the source stage for CodePipeline
     const sourceStage = {
       stageName: "Source",
-      actions: [
-        new pipelineactions.GitHubSourceAction({
-          actionName: "GitHubSource",
-          owner: 'ppatram',
-          repo: 'my-pipeline',
-          branch: 'main',
-          oauthToken: gitHubToken.secretValueFromJson('token'),
-          output: sourceArtifact
-        }),
-      ],
+      actions: [githubSourceAction],
     };
 
-    /*
-    const codeRepo = new github.GitHubSourceAction({
-      owner: 'ppatram',
-      repo: 'my-pipeline',
-      branch: 'main',
-      actionName: 'gitHubSource',
-      oauthToken: gitHubToken.secretValueFromJson('token'),
-      output: new pipeline.Artifact(),
-      runOrder: 1,
-      //trigger: github.GitHubTrigger.WEBHOOK
-       });
-    */
+
+    // CodeBuild project that builds the Docker image
+    const buildProject = new codebuild.Project(this, "buildProject", {
+      source: codebuild.Source.gitHub({
+        owner: 'ppatram',
+        repo: 'my-pipeline',
+        branchOrRef: 'main',
+       
+      })
+    });     
+
 
     // Run jest test and send result to CodeBuild    
     const testStage = {
@@ -283,62 +81,17 @@ export class CodepipelineBuildDeployStack extends cdk.Stack {
         new pipelineactions.CodeBuildAction({
           actionName: "JestCDK",
           input: new pipeline.Artifact("SourceArtifact"),
-          project: buildTest,
-        }),
-      ],
-    };
-
-    // Creates the build stage for CodePipeline
-    const buildStage = {
-      stageName: "Build",
-      actions: [
-        new pipelineactions.CodeBuildAction({
-          actionName: "DockerBuildPush",
-          input: new pipeline.Artifact("SourceArtifact"),
-          project: buildImage,
-          outputs: [buildArtifact],
-        }),
-      ],
-    };
-
-    // Creates a new CodeDeploy Deployment Group
-    const deploymentGroup = new codedeploy.EcsDeploymentGroup(
-      this,
-      "CodeDeployGroup",
-      {
-        service: fargateService,
-        // Configurations for CodeDeploy Blue/Green deployments
-        blueGreenDeploymentConfig: {
-          listener: albListener,
-          blueTargetGroup: targetGroupBlue,
-          greenTargetGroup: targetGroupGreen,
-        },
-      }
-    );
-
-    // Creates the deploy stage for CodePipeline
-    const deployStage = {
-      stageName: "Deploy",
-      actions: [
-        new pipelineactions.CodeDeployEcsDeployAction({
-          actionName: "EcsFargateDeploy",
-          appSpecTemplateInput: buildArtifact,
-          taskDefinitionTemplateInput: buildArtifact,
-          deploymentGroup: deploymentGroup,
+          project: buildProject,
         }),
       ],
     };
 
     // Creates an AWS CodePipeline with source, build, and deploy stages
-    new pipeline.Pipeline(this, "BuildDeployPipeline", {
-      pipelineName: "ImageBuildDeployPipeline",
-      stages: [sourceStage, testStage, buildStage, deployStage],
+    new pipeline.Pipeline(this, "BuildDeployPipelineTest", {
+      pipelineName: "ImageBuildDeployPipelineTest",
+      stages: [sourceStage, testStage],
     });
 
-    // Outputs the ALB public endpoint
-    new cdk.CfnOutput(this, "PublicAlbEndpoint", {
-      value: "http://" + publicAlb.loadBalancerDnsName,
-    });
     
   }
 
--- test/orig_codepipeline-build-deploy.test.ts
+++ test/orig_codepipeline-build-deploy.test.ts
@@ -1,11 +1,11 @@
 import * as cdk from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
-import { CodepipelineBuildDeployStackTest } from "../lib/codepipeline-build-deploy-stack";
+import { CodepipelineBuildDeployStack } from "../lib/codepipeline-build-deploy-stack";
 
 // Checks if the ECS Deployment Controller is set to AWS CodeDeploy
 test("Deployment Controller Set", () => {
   const app = new cdk.App();
-  const stack = new CodepipelineBuildDeployStackTest(app, "MyTestStack");
+  const stack = new CodepipelineBuildDeployStack(app, "MyTestStack");
   const template = Template.fromStack(stack);
 
   template.hasResourceProperties("AWS::ECS::Service", {
@@ -18,7 +18,7 @@ test("Deployment Controller Set", () => {
 // Checks if the ALB Security Group allows all traffic on port 80
 test("Security Group Port 80 Open", () => {
   const app = new cdk.App();
-  const stack = new CodepipelineBuildDeployStackTest(app, "MyTestStack");
+  const stack = new CodepipelineBuildDeployStack(app, "MyTestStack");
   const template = Template.fromStack(stack);
 
   template.hasResourceProperties("AWS::EC2::SecurityGroup", {
@@ -36,7 +36,7 @@ test("Security Group Port 80 Open", () => {
 // Checks if public access to the S3 Bucket is disabled
 test("S3 Bucket Restricted", () => {
   const app = new cdk.App();
-  const stack = new CodepipelineBuildDeployStackTest(app, "MyTestStack");
+  const stack = new CodepipelineBuildDeployStack(app, "MyTestStack");
   const template = Template.fromStack(stack);
 
   template.hasResourceProperties("AWS::S3::Bucket", {
